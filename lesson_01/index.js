5d99bed98e1490e60f327af9ab377fe7dbce3b20
Alex <af88_88@mail.ru>
2022-04-27 19:02:11 +0300

    добавил стартовые файлы

---
+++ lesson_01/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const productName = 'car';
+const quantityOfGoods = 10;
+const productCategory = 'passenger cars';
+const priceOfGoods = 1000;
+const totalPrice = quantityOfGoods * priceOfGoods;
+
+console.log(productName);
+console.log(totalPrice);
+
