918965fe3b62ced973af8c8f20eab68d442c3ffd
Alex <af88_88@mail.ru>
2022-04-27 19:07:11 +0300

    решение д/з 2

---
+++ lesson_02/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const productName = 'моющая машина';
+const quantityOfGoods = 85;
+const productCategory = 'бытовая техника';
+const priceOfGoods = 450;
+
+
+console.log(`Общая сумма товара: ${quantityOfGoods * priceOfGoods}`);
+
