98306280440cd5d5cb5a602613529e06965ed9c4
Alex <af88_88@mail.ru>
2022-04-29 16:46:03 +0300

    lesson_03

---
+++ lesson_03/einstein.js
@@ -0,0 +1,4 @@
+const myWeight = 15;
+const speedLight = 3e8;
+
+console.log(myWeight * speedLight);
---
+++ lesson_03/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const productName = prompt("Наименование товара:","");
+const quantityOfGoods = prompt("Количество товара:")
+const productCategory = prompt("Категория товара:")
+const priceOfGoods = prompt("Цена товара:")
+
+
+console.log(`"На складе ${quantityOfGoods} единицы товара ${productName}, на сумму ${quantityOfGoods * priceOfGoods} деревянных`);
+
