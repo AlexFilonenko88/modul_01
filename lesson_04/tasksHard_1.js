1bb125f32043db3568a5b1a55efee3a904a13495
Alex <af88_88@mail.ru>
2022-04-30 18:08:04 +0300

    изм после проверки куратором

--- lesson_04/tasksHard_1.js
+++ lesson_04/tasksHard_1.js
@@ -2,20 +2,20 @@
 
 // 1
 
-const income = prompt("Введите Ваш доход: ");
+const income = +prompt("Введите Ваш доход: ");
 const TAX13 = 0.13;
 const TAX20 = 0.2;
 const TAX30 = 0.3;
 
 if (income < 15000) {
     console.log(income * TAX13);
-} else if (income >= 15000 && income < 50000) {
+} if (income >= 15000 && income < 50000) {
     console.log(income * TAX20)
-} else {
+} if(income >= 50000) {
     console.log(income * TAX30);
 };
 
-// switch (income) {
+// switch (true) {
 //     case income < 15000:
 //         console.log(income * TAX13);
 //         break;
@@ -25,4 +25,6 @@ if (income < 15000) {
 //     case income >= 50000:
 //         console.log(income * TAX30);
 //         break;
+//     default:
+//         console.log('default');
 // };
