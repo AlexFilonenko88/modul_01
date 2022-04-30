88a2de3f509fc97e3970e5d6d75ac89a98fb6885
Alex <af88_88@mail.ru>
2022-04-30 15:40:52 +0300

    первые 4 задачи

--- lesson_04/index.js
+++ lesson_04/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const productName = prompt("Наименование товара:","");
-const quantityOfGoods = prompt("Количество товара:")
-const productCategory = prompt("Категория товара:")
-const priceOfGoods = prompt("Цена товара:")
+const quantityOfGoods = prompt("Количество товара:");
+const productCategory = prompt("Категория товара:");
+const priceOfGoods = prompt("Цена товара:");
 
 if (typeof quantityOfGoods === 'number' && typeof priceOfGoods === 'number') {
     console.log(`На складе ${quantityOfGoods} единицы товара ${productName}, на сумму ${quantityOfGoods * priceOfGoods} деревянных`);
--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -1,9 +1,36 @@
 'use strict'
 
+// 1
+
 const rain = Math.round(Math.random());
 
 if (rain === 1) {
     console.log("Пошёл дождь. Возьмите зонт!");
 } else {
     console.log("Дождя нет!");
+}
+
+// 2
+
+const numberOfPointsMath = prompt("Введите кол-во баллов по математике:");
+const numberOfPointsRussianLanguage = prompt("Введите кол-во баллов по русскому языку:");
+const numberOfPointsComputerScience = prompt("ведите кол-во баллов по информатике:");
+const totalPoints = numberOfPointsMath + numberOfPointsRussianLanguage + numberOfPointsComputerScience;
+const POINTS = 265;
+
+if (totalPoints >= POINTS) {
+    alert("Поздравляю, вы поступили на бюджет!");
+} else {
+    alert("Не достаточно баллов!");
+};
+
+// 3
+
+const cash = prompt("Какую сумму желаете снять?");
+const MINBANKNOTE = 100; 
+
+if (cash >= MINBANKNOTE) {
+    console.log("Получите ваши деньги!")
+} else {
+    console.log("Неправелнный ввод. Минимальная купюра 100р.")
 }
