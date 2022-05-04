df3e0d97a0b96a339097b97067e4c1b8caf63e65
Alex <af88_88@mail.ru>
2022-05-04 16:37:10 +0300

    пятая задача

--- lesson_08/game01.js
+++ lesson_08/game01.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const userNumber = +prompt("Введите любое число:");
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
+
+const y = (userNumber) => {
+    if (userNumber > randomNumber) {
+        alert("Меньше!");
+        const userNumber = +prompt("Введите любое число:");
+    } else if (userNumber < randomNumber) {
+        alert("Больше!");
+        userNumber = +prompt("Введите любое число:");
+    } else if (userNumber === randomNumber) {
+        alert("Правильно!");
+    } else {
+        alert("Введите число!");
+    }
+};
+
+console.log(y(userNumber));
