1528a38d97898357e4d78e8fb8b4730ff03aa701
Alex <af88_88@mail.ru>
2022-05-05 12:01:35 +0300

    доп задача

--- lesson_08/game02.js
+++ lesson_08/game02.js
@@ -10,22 +10,30 @@
 const userNumber = +prompt("Введите любое число:");
 const randomNumber = Math.round(Math.random() * 100);
 const inputUserNumber = [];
+let x = inputUserNumber.join('');
+let q = Number(x);
 
 const y = (userNumber) => {
 
-        if (userNumber > randomNumber) {
-            alert("Меньше!");
-            const userNumber = +prompt("Введите любое число:");
-            inputUserNumber.push(userNumber);
-        } else if (userNumber < randomNumber) {
-            alert("Больше!");
-            userNumber = +prompt("Введите любое число:");
-            inputUserNumber.push(userNumber);
-        } else if (userNumber === randomNumber) {
-            alert("Правильно!");
-        } else {
-            alert("Введите число!");
-        }
+    if (userNumber === randomNumber * 0.3) { // ???
+        return; 
+    } if (userNumber > randomNumber) {
+        inputUserNumber.push(userNumber);
+        alert("Меньше!");
+        const userNumber = +prompt("Введите любое число:");
+    } if (userNumber < randomNumber) {
+        alert("Больше!");
+        userNumber = +prompt("Введите любое число:");
+    } if (userNumber === randomNumber) {
+        alert("Правильно!");
+    } if (userNumber === q) {   // ???
+        alert("Это число вы уже вводили");
+    } if (randomNumber >= 50 && randomNumber <= 100) { // ???
+        
+    }
+    else {
+        alert("Введите число!");
+    }
 
 };
 
